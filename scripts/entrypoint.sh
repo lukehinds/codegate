51ce5bba43e2b0e6a00e92ea5c85a5fc11b97c42
Luke Hinds <luke@stacklok.com>
2024-12-29 09:54:12 +0000

    sqlite-vec

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,25 +1,9 @@
 #!/bin/bash
 
 # those are hardcoded on the image, will not change
-BACKUP_PATH="/tmp/weaviate_backup"
-BACKUP_NAME="backup"
-MODEL_BASE_PATH="/app/codegate_volume/models"
 CODEGATE_DB_FILE="/app/codegate_volume/db/codegate.db"
+CODEGATE_VEC_DB_FILE="/app/sqlite_data/vectordb.db"
 CODEGATE_CERTS="/app/codegate_volume/certs"
-
-# Function to restore backup if paths are provided
-restore_backup() {
-    if [ -n "$BACKUP_PATH" ] && [ -n "$BACKUP_NAME" ]; then
-        if [ -d "$BACKUP_PATH" ] && [ -d "$BACKUP_PATH/$BACKUP_NAME" ]; then
-            echo "Restoring backup from $BACKUP_PATH/$BACKUP_NAME..."
-            python -m src.codegate.cli restore-backup --backup-path "$BACKUP_PATH" --backup-name "$BACKUP_NAME"
-        else
-            echo "No backup found at $BACKUP_PATH/$BACKUP_NAME. Skipping restore."
-        fi
-    else
-        echo "Backup path or mode not provided. Skipping restore."
-    fi
-}
 
 genrerate_certs() {
     echo "Generating certificates..."
@@ -37,7 +21,7 @@ start_application() {
     # first restore the models
     mkdir -p /app/codegate_volume/models
     cp /app/default_models/* /app/codegate_volume/models
-    CMD_ARGS="--port 8989 --host 0.0.0.0 --model-base-path $MODEL_BASE_PATH --db-path $CODEGATE_DB_FILE"
+    CMD_ARGS="--port 8989 --host 0.0.0.0 --model-base-path $MODEL_BASE_PATH --db-path $CODEGATE_DB_FILE --vec-db-path $CODEGATE_VEC_DB_FILE"
 
     # Check and append additional URLs if they are set
     [ -n "$CODEGATE_OPENAI_URL" ] && CMD_ARGS+=" --openai-url $CODEGATE_OPENAI_URL"
@@ -55,10 +39,6 @@ start_application() {
 
 # Main execution flow
 echo "Initializing entrypoint script..."
-
-
-# Step 1: Restore backup if applicable
-restore_backup
 
 # Step 2: Generate certificates
 genrerate_certs
